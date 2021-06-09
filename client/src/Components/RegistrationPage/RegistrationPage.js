e2c617064efa4dbd473a427495c3e4b0c437c512
Clark <clarkhyde123@gmail.com>
2021-06-09 13:22:49 -0500

    intial skeleton of registration page is created

--- client/src/Components/RegistrationPage/RegistrationPage.js
+++ client/src/Components/RegistrationPage/RegistrationPage.js
@@ -1,11 +1,76 @@
 import React from 'react';
 import './RegistrationPage.scss';
+import { Link } from 'react-router-dom';
 
 
 const RegistrationPage = () => {
     return (
         <div>
-            Hello world!
+            {/*Probably the location of a header or maybe a nav componenent */}
+            <h1 className="">Register</h1>
+            <form
+                className=""
+            >
+                <label 
+                    className="" 
+                    htmlFor="username">
+                        UserName: 
+                </label>
+                <input 
+                    type="text" 
+                    className="" 
+                    name="username"
+                    placeholder="Enter username here"
+                />
+                <label
+                    className=""
+                    htmlFor="password"
+                >
+                    Password:
+                </label>
+                <input 
+                    type="text"
+                    className=""
+                    name="password"
+                    placeholder="Enter Password"
+                />
+                                <label
+                    className=""
+                    htmlFor="confirmPassword"
+                >
+                    Confirm Password:
+                </label>
+                <input 
+                    type="text"
+                    className=""
+                    name="confirmPassword"
+                    placeholder="Confirm Password"
+                />
+                <label
+                    type="text"
+                    className=""
+                    htmlFor="goals"
+                >
+                    Enter Goals:
+                </label>
+                <input 
+                    type="text"
+                    className=""
+                    name="goals"
+                    placeholder="Enter goals if applicable."
+                />
+                <button
+                    className=""
+                    type=""> 
+                    Register
+                </button>
+            </form>
+            <Link 
+                className=""
+                to="/login"
+            > 
+                Already Registered? Click here to login
+            </Link>
         </div>
     );
 };
