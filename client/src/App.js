72ddf17757203583e39b37e8d022db97ad11992c
Clark <clarkhyde123@gmail.com>
2021-06-10 12:03:25 -0500

    set up route for exercise details page and basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from './Components/LandingPage/LandingPage';
 import RegistrationPage from './Components/RegistrationPage/RegistrationPage';
 import LoginPage from './Components/LoginPage/LoginPage.js';
+import ExerciseDetails from './Components/IndividualExercise/ExerciseDetails';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route exact path = "/" component = {LandingPage}/>
           <Route path = "/register" component = {RegistrationPage}/>
           <Route path = "/login" component = {LoginPage}/> 
+          <Route path = "/exercisedetails" component = {ExerciseDetails}/>
         </Switch>
       </BrowserRouter>
     </body>
---
+++ client/src/Components/IndividualExercise/ExerciseDetails.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+const ExerciseDetails = () => {
+    return (
+        <div>
+            <h1>Exercise Details</h1>
+            <ul className = "">
+                <li className = "">NAME.. Barbell Flat Bench Press</li>
+                <li className = "">Body Part.. Chest</li>
+                <li className = "">Type.. Freeweight</li>
+                <li className = "">Equipment.. Flat bench, Barbell, plates</li>
+                <li className = "">Notes.. can adjust group to change how you hit the chest and triceps. Closer hits triceps more, wider forcuses on chest. </li>
+            </ul>
+
+            <div> Edit</div>
+            <div>Back</div>
+            <div>logout</div>
+        </div>
+    );
+};
+
+export default ExerciseDetails;
