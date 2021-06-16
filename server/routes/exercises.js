82aac87511f76503d358747baeabf2435b75cb42
Clark <clarkhyde123@gmail.com>
2021-06-16 16:03:15 -0500

    routes to get exercises by type and body part set up and working

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const Exercises = require("../models/exercises");
 
 
-
-
 //GET ALL EXERCISES
 router.get('/exercises', (req, res) => {
     Exercises
@@ -17,5 +15,33 @@ router.get('/exercises', (req, res) => {
             return res.status(404).json({ error: "an error has occured. Please make another attempt." })
         });
 });
+
+//GET EXERCISES BY BODYPART
+router.get('/exercises/bodypart/:bodypart', (req, res) => {
+    Exercises
+        .where("bodyPart", req.body.bodypart)
+        .fetchAll()
+        .then(exercises => {
+            res.status(200).json(exercises);
+            console.log("sent exercises for that particular body part" + exercises);
+        })
+        .catch(() => {
+            return res.status(404).json({ error: "ann error has occured. Please make another attempt." })
+        });
+});
+
+//GET EXERCISES BY TYPE
+router.get('/exercises/type/:type', (req, res) => {
+    Exercises
+        .where("type", req.body.type)
+        .fetchAll()
+        .then(exercises => {
+            res.status(200).json(exercises);
+            console.log("sent exercises for that particular type" + exercises);
+        })
+        .catch(() => {
+            return res.status(404).json({ error: "ann error has occured. Please make another attempt." })
+        });
+})
 
 module.exports = router;
